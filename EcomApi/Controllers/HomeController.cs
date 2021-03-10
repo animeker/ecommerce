ee924a6060b879aaa3b555edab8fa3915f8fd811
Michelle Lee <michellechulee@gmail.com>
2021-03-10 03:23:49 -0800

    displayed data and notes

--- EcomApi/Controllers/HomeController.cs
+++ EcomApi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EcomApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,22 @@ using System.Web.Mvc;
 
 namespace EcomApi.Controllers
 {
+
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
+            ProductsController products = new ProductsController();
+
+            // ViewBag enables sharing of values dynamically between the Controller and View
+            ViewBag.Name = "JohnDoe";
+            ViewBag.Product1 = products.Get(1);
+            // ViewBag.Products = products.Get();
+
+            // View() returns the name of the action method which is the index, "ActionResult Index()"
             return View();
         }
+
     }
 }
