e29c9817a8261fe07c81cf2425d63c00b80bff5c
Michelle Lee <michellechulee@gmail.com>
2021-03-14 21:36:16 -0700

    fixed routing for TestTest page

--- EcomApi/Controllers/ProductsController.cs
+++ EcomApi/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 using EcomApi.Models;
 using Newtonsoft.Json;
 
@@ -12,6 +13,16 @@ namespace EcomApi.Controllers
 {
     public class ProductsController : ApiController
     {
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        private ActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+
         OnlineStoreEntities db = new OnlineStoreEntities();
 
         // GET api/Products
